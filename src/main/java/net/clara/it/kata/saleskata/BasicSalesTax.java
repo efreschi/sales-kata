81ad7dd9c9e6199db7489a91c44746bc06fb070a
elisabettafreschi <elisabettafreschi@mbp-di-claranet.lan>
2022-04-26 18:56:29 +0200

    Introdotto gestione delle tipologie di prodotti esenti da Basic Sales Tax

--- src/main/java/net/clara/it/kata/saleskata/BasicSalesTax.java
+++ src/main/java/net/clara/it/kata/saleskata/BasicSalesTax.java
@@ -1,10 +1,42 @@
 package net.clara.it.kata.saleskata;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import net.clara.it.kata.saleskata.Item.ItemType;
 
 public class BasicSalesTax extends Tax {
 	
 	private static final BigDecimal RATE = new BigDecimal("0.1");
+
+	private Set<ItemType> exemptTypes = new HashSet<>();
+	
+	public BasicSalesTax(Set<ItemType> exemptTypes) {
+		super();
+		if (exemptTypes != null) {
+		this.exemptTypes.addAll(exemptTypes);
+			
+		}
+	}
+	
+	
+
+	@Override
+	public double apply(Item item) {
+		if (!isItemExempted(item)) {
+			return super.apply(item);
+		}
+		return 0D;
+	}
+
+
+
+	protected boolean isItemExempted(Item item) {
+		return exemptTypes.contains(item.getType());
+	}
+
+
 
 	@Override
 	protected BigDecimal getRate() {
--- src/test/java/net/clara/it/kata/saleskata/BasicSalesTaxTest.java
+++ src/test/java/net/clara/it/kata/saleskata/BasicSalesTaxTest.java
@@ -5,21 +5,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class BasicSalesTaxTest {
+	
 
 	@Test
 	public void testObjectPriceTaxInput1() {
 		Item item = new Item("book", Item.ItemType.BOOK, false, 12.49);
 		
-		Tax basicSalesTax = new BasicSalesTax();
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
 		double tax = basicSalesTax.apply(item);
-		assertThat(tax).isEqualTo(1.25);
+		assertThat(tax).isEqualTo(0D);
 	}
 
 	@Test
 	public void testBasicTaxTaxInput2() {
 		Item item = new Item("imported bottle of perfume", Item.ItemType.OTHER, false, 47.5);
 		
-		Tax basicSalesTax = new BasicSalesTax();
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
 		double tax = basicSalesTax.apply(item);
 		assertThat(tax).isEqualTo(4.75);
 	}
@@ -28,8 +29,8 @@ public class BasicSalesTaxTest {
 	public void testObjectPriceImportedTaxInput1() {
 		Item item = new Item("book", Item.ItemType.BOOK, true, 12.49);
 		
-		Tax basicSalesTax = new BasicSalesTax();
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
 		double tax = basicSalesTax.apply(item);
-		assertThat(tax).isEqualTo(1.25);
+		assertThat(tax).isEqualTo(0D);
 	}
 }
