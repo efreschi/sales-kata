30e86ebce237b28c82a58464766edeb1bfaed015
elisabettafreschi <elisabettafreschi@mbp-di-claranet.lan>
2022-04-26 23:24:35 +0200

    Aggiunto classe di test per calcolare item tax

---
+++ src/test/java/net/clara/it/kata/saleskata/ItemTaxTest.java
@@ -0,0 +1,38 @@
+package net.clara.it.kata.saleskata;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ItemTaxTest {
+	
+	@Test
+	public void testItemTaxForImportedBottlePerfume() {
+		Item item = 
+				new Item("imported bottle of perfume", Item.ItemType.OTHER, true, 47.5);
+		ItemTax taxItem = new ItemTax();
+		double tax = taxItem.apply(item);
+		assertThat(tax).isEqualTo(7.15);
+		
+	}
+
+	@Test
+	public void testItemTaxForBook() {
+		Item item = 
+				new Item("book", Item.ItemType.BOOK, false, 12.49);
+		ItemTax taxItem = new ItemTax();
+		double tax = taxItem.apply(item);
+		assertThat(tax).isEqualTo(0D);
+		
+	}
+
+	@Test
+	public void testItemTaxForMusicCDk() {
+		Item item = 
+				new Item("music CD", Item.ItemType.OTHER, false, 14.99);
+		ItemTax taxItem = new ItemTax();
+		double tax = taxItem.apply(item);
+		assertThat(tax).isEqualTo(1.5);
+		
+	}
+}
