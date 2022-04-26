ced0c25a6fa8b7887937725b51f18e0bf0848701
elisabettafreschi <elisabettafreschi@mbp-di-claranet.lan>
2022-04-26 23:16:46 +0200

    Aggiunti casi di test

--- src/test/java/net/clara/it/kata/saleskata/BasicSalesTaxTest.java
+++ src/test/java/net/clara/it/kata/saleskata/BasicSalesTaxTest.java
@@ -8,7 +8,7 @@ public class BasicSalesTaxTest {
 	
 
 	@Test
-	public void testObjectPriceTaxInput1() {
+	public void testBasicSalexTaxForBook() {
 		Item item = new Item("book", Item.ItemType.BOOK, false, 12.49);
 		
 		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
@@ -17,8 +17,8 @@ public class BasicSalesTaxTest {
 	}
 
 	@Test
-	public void testBasicTaxTaxInput2() {
-		Item item = new Item("imported bottle of perfume", Item.ItemType.OTHER, false, 47.5);
+	public void testBasicSalexTaxForImportedPerfume() {
+		Item item = new Item("imported bottle of perfume", Item.ItemType.OTHER, true, 47.5);
 		
 		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
 		double tax = basicSalesTax.apply(item);
@@ -26,8 +26,44 @@ public class BasicSalesTaxTest {
 	}
 
 	@Test
-	public void testObjectPriceImportedTaxInput1() {
+	public void testBasicSalexTaxForImportedBook() {
 		Item item = new Item("book", Item.ItemType.BOOK, true, 12.49);
+		
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(0D);
+	}
+
+	@Test
+	public void testBasicSalexTaxForMusicCD() {
+		Item item = new Item("music CD", Item.ItemType.OTHER, false, 14.99);
+		
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(1.5);
+	}
+
+	@Test
+	public void testBasicSalexTaxForChocolateBar() {
+		Item item = new Item("chocolate bar", Item.ItemType.FOOD, false, 0.85);
+		
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(0D);
+	}
+
+	@Test
+	public void testBasicSalexTaxForBottlePerfume() {
+		Item item = new Item("bottle of perfume", Item.ItemType.OTHER, false, 47.5);
+		
+		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(4.75);
+	}
+
+	@Test
+	public void testBasicSalexTaxPackectHeadachePills() {
+		Item item = new Item("packet of headache pills", Item.ItemType.MEDICAL, false, 9.75);
 		
 		Tax basicSalesTax = new BasicSalesTax(ItemTax.getExemptTypes());
 		double tax = basicSalesTax.apply(item);
--- src/test/java/net/clara/it/kata/saleskata/ImportDutyTaxTest.java
+++ src/test/java/net/clara/it/kata/saleskata/ImportDutyTaxTest.java
@@ -7,7 +7,34 @@ import org.junit.jupiter.api.Test;
 public class ImportDutyTaxTest {
 
 	@Test
-	public void testImportedDutyTaxInput1() {
+	public void testImportedDutyTaxForBook() {
+		Item item = new Item("book", Item.ItemType.BOOK, false, 10.00);
+		
+		Tax basicSalesTax = new ImportDutyTax();
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(0D);
+	}
+
+	@Test
+	public void testImportedDutyTaxForMusicCD() {
+		Item item = new Item("music CD", Item.ItemType.OTHER, false, 14.99);
+		
+		Tax basicSalesTax = new ImportDutyTax();
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(0D);
+	}
+
+	@Test
+	public void testImportedDutyTaxForChocolateBar() {
+		Item item = new Item("chocolate bar", Item.ItemType.FOOD, false, 14.99);
+		
+		Tax basicSalesTax = new ImportDutyTax();
+		double tax = basicSalesTax.apply(item);
+		assertThat(tax).isEqualTo(0D);
+	}
+	
+	@Test
+	public void testImportedDutyTaxForImportedChocolate() {
 		Item item = new Item("imported box of chocolates", Item.ItemType.FOOD, true, 10.00);
 		
 		Tax basicSalesTax = new ImportDutyTax();
@@ -16,20 +43,11 @@ public class ImportDutyTaxTest {
 	}
 
 	@Test
-	public void testImportedDutyTaxInput2() {
+	public void testImportedDutyTaxForImportedPerfume() {
 		Item item = new Item("imported bottle of perfume", Item.ItemType.OTHER, true, 47.5);
 		
 		Tax basicSalesTax = new ImportDutyTax();
 		double tax = basicSalesTax.apply(item);
 		assertThat(tax).isEqualTo(2.4);
-	}
-
-	@Test
-	public void testNotImportedDutyTaxInput3() {
-		Item item = new Item("imported box of chocolates", Item.ItemType.FOOD, false, 10.00);
-		
-		Tax basicSalesTax = new ImportDutyTax();
-		double tax = basicSalesTax.apply(item);
-		assertThat(tax).isEqualTo(0D);
 	}
 }
