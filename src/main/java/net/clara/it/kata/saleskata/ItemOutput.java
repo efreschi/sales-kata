3d56ac0355c7f6012e6f76a4a60b2d7890b5ec43
elisabettafreschi <elisabettafreschi@mbp-di-claranet.lan>
2022-04-26 23:48:20 +0200

    Evoluto calcolo output singolo item e relativi test

--- src/main/java/net/clara/it/kata/saleskata/ItemOutput.java
+++ src/main/java/net/clara/it/kata/saleskata/ItemOutput.java
@@ -1,9 +1,24 @@
 package net.clara.it.kata.saleskata;
 
-public class ItemOutput {
+import java.math.BigDecimal;
 
-	public String output(Item item) {
-		return item.getName() + ": " + item.getPrice();
+public class ItemOutput {
+	
+	private ItemTax itemTax;
+
+	public ItemOutput(ItemTax itemTax) {
+		super();
+		this.itemTax = itemTax;
+	}
+
+	public String output(int card, Item item) {
+		return card + " " + item.getName() + ": " + getItemTotalPrice(card, item);
+	}
+	
+	protected double getItemTotalPrice(int card, Item item) {
+		return new BigDecimal("" + card)
+				.multiply(new BigDecimal("" + item.getPrice()).add(new BigDecimal("" + itemTax.apply(item))))
+				.doubleValue();
 	}
 
 }
--- src/test/java/net/clara/it/kata/saleskata/ItemOutputTest.java
+++ src/test/java/net/clara/it/kata/saleskata/ItemOutputTest.java
@@ -7,11 +7,42 @@ import org.junit.jupiter.api.Test;
 public class ItemOutputTest {
 	
 	@Test
-	public void test_InstanceGoodAndOutput() {
-		Item item = new Item("book", Item.ItemType.BOOK, true, 12.49);
-		ItemOutput itemOutput = new ItemOutput();
-		String res = itemOutput.output(item);
-		assertThat(res).isEqualToIgnoringCase("book: 12.49");
+	public void testItemOutputForBook() {
+		Item item = new Item("book", Item.ItemType.BOOK, false, 12.49);
+		ItemOutput itemOutput = new ItemOutput(new ItemTax());
+		String res = itemOutput.output(1, item);
+		assertThat(res).isEqualToIgnoringCase("1 book: 12.49");
 	}
 
+	@Test
+	public void testItemOutputFor2Book() {
+		Item item = new Item("book", Item.ItemType.BOOK, false, 12.49);
+		ItemOutput itemOutput = new ItemOutput(new ItemTax());
+		String res = itemOutput.output(2, item);
+		assertThat(res).isEqualToIgnoringCase("2 book: 24.98");
+	}
+
+	@Test
+	public void testItemOutputForMusicCD() {
+		Item item = new Item("music CD", Item.ItemType.OTHER, false, 14.99);
+		ItemOutput itemOutput = new ItemOutput(new ItemTax());
+		String res = itemOutput.output(1, item);
+		assertThat(res).isEqualToIgnoringCase("1 music CD: 16.49");
+	}
+
+	@Test
+	public void testItemOutputFor3BoxOfImportedChocolate() {
+		Item item = new Item("box of imported chocolates", Item.ItemType.FOOD, true, 11.25);
+		ItemOutput itemOutput = new ItemOutput(new ItemTax());
+		String res = itemOutput.output(3, item);
+		assertThat(res).isEqualToIgnoringCase("3 box of imported chocolates: 35.55");
+	}
+
+	@Test
+	public void testItemOutputForBottlePerfume() {
+		Item item = new Item("bottle of perfume", Item.ItemType.OTHER, false, 18.99);
+		ItemOutput itemOutput = new ItemOutput(new ItemTax());
+		String res = itemOutput.output(1, item);
+		assertThat(res).isEqualToIgnoringCase("1 bottle of perfume: 20.89");
+	}
 }
